53c8af45403365912ef2f1a017892ac5e98d6702
patricia7pop <115619135+patricia7pop@users.noreply.github.com>
2022-10-18 22:07:37 +0300

    Update gamma.js

--- gamma.js
+++ gamma.js
@@ -1,5 +1,6 @@
-window.addEventListener("deviceorientation", on_device_orientation);
-function on_device_orientation(evt){
+window.addEventListener("deviceorientation",on_device_orientation);
+function on_device_orientation(evt)
+{
   var alpha = evt.alpha;
   var beta = evt.beta;
   var gamma = evt.gamma;
@@ -8,13 +9,11 @@ function on_device_orientation(evt){
   document.getElementById("c").innerHTML = "gamma = "+gamma;
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
-  var centru = {x:canvas.width/2, z:canvas.height/2}
+  var centru = {x:canvas.width/2, y:canvas.height/2}
   var latura_patrat = 50;
-  context.setTransform(1, 0, 0, 1, 0, 0);
-  context.clearRect(0,0,canvas.width, canvas.height);
-  context.translate(centru.x/2, centru.z/2);
-  context.rotate(gamma * Math.PI / 90);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "#6A0888";
   context.beginPath();
-  context.rect(-latura_patrat/2,-latura_patrat/2, latura_patrat, latura_patrat);
-  context.stroke();
+  context.fillRect(180, 150, evt.gamma, 50);
 }
+
